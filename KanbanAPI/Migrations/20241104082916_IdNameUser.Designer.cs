cdd45ae5599ba36fce1db6cf4bb860ad9c82c595
Tintin Falk <tintinfalk01@gmail.com>
2024-11-04 09:32:35 +0100

    usercontroller + userdto

--- KanbanAPI/Migrations/20241104082916_IdNameUser.Designer.cs
+++ KanbanAPI/Migrations/20241104082916_IdNameUser.Designer.cs
@@ -4,6 +4,7 @@ using KanbanAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KanbanAPI.Migrations
 {
     [DbContext(typeof(KanbanContext))]
-    partial class KanbanContextModelSnapshot : ModelSnapshot
+    [Migration("20241104082916_IdNameUser")]
+    partial class IdNameUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
